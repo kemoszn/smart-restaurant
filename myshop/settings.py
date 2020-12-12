c2b708edcd1cc10e1b6240c5f63061826994905f
Metro <mohdkamal47@gmail.com>
2020-12-12 11:44:30 +0200

    static files config for windows

--- myshop/settings.py
+++ myshop/settings.py
@@ -121,12 +121,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # MEDIAFILES
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # CART 
 CART_SESSION_ID = 'cart'
